1bd6fdc00ec3faf33bb7c381480f985eeb23f3af
coding-ji <eunjikwon0530@hotmail.com>
2024-12-26 12:14:13 +0900

    나.아.가.자.해.내.자

---
+++ src/test/java/com/kosmo/pitchplay/PitchplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosmo.pitchplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitchplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
