e5f55413f277b8be967121ee9b6bd6da51652b60
KOSMO <yeonhui94@icloud.com>
2025-01-02 17:46:54 +0900

    노티스 업데이트~~ 더미데이터 완료

--- src/main/java/com/kosmo/pitchplay/controller/NoticeController.java
+++ src/main/java/com/kosmo/pitchplay/controller/NoticeController.java
@@ -13,7 +13,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/notices")
 @RequiredArgsConstructor
-
 public class NoticeController {
 
     private final NoticeService noticeService;
